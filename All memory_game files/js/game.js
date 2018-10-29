1aa054df6fa880f58fd8e2f7b4271df459e504ca
Sultan Alsubaie <Alsubaie.sultan.b@gmail.com>
2018-10-29 19:12:59 +0300

    created game logic

---
+++ All memory_game files/js/game.js
@@ -0,0 +1,9 @@
+
+
+var cardOne = "qeen";
+var cardTwo = "qeen";
+var cardThree = "King";
+var cardFour = "King";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
