0fd59ededa10cb2ec0e77c300251f6dc9d949ccc
Sultan Alsubaie <Alsubaie.sultan.b@gmail.com>
2018-11-01 03:00:39 +0300

    created game logic

--- All memory_game files/js/game.js
+++ All memory_game files/js/game.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+    {
+        rank: "Queen",
+        suit: "Hearts",
+        cardImage:"images/queen-of-hearts.png"
+    },
+    {
+        rank: "Queen",
+        suit: "Diamonds",
+        cardImage:"images/queen-of-diamonds.png"
+    },
+    {
+        rank: "King",
+        suit: "Hearts",
+        cardImage:"images/king-of-hearts.png"
+    },
+    {
+        rank: "King",
+        suit: "Diamonds",
+        cardImage:"images/king-of-diamonds.png"
+    } 
+];
 
 var cardsInPlay = [];
 
@@ -14,9 +35,11 @@ var checkForMatch = function(){
 
 var flipCard = function (cardId) {
 
-   console.log("User flipped " + cards[cardId]);
+   console.log("User flipped " + cards[cardId].rank);
+   console.log(cards[cardId].cardImage);
+   console.log(cards[cardId].suit);
    
-   cardsInPlay.push(cards[cardId]);
+   cardsInPlay.push(cards[cardId].rank);
 
    checkForMatch();
 
